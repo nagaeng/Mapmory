038edb0505ff4811c93f486995fac91c24ebcb79
maeng <20221153@sungshin.ac.kr>
2023-08-06 20:11:20 +0900

    feat: 해시태그선택기능

--- Mapmory/post/views.py
+++ Mapmory/post/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Post,Hashtag
+from .forms import PostForm
 
-# Create your views here.
+def select_hashtag(request):
+    hashtag = Hashtag.objects.all()
+    if request.method == 'POST':
+        select_hashtag = request.POST.getlist('hashtag')
+        request.sessions['select_hashtag']=select_hashtag
+        return redirect('post')
+    return render(request, 'hashtag.html', {'hashtag':hashtag})
+
+def create(request, User_id):
+    print(User)
+    user = get_object_or_404(User, pk=User_id)
+    User_id = User_id
+
+    select_hashtag = request.sessions.get('select_hashtag',[])
+
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.writer = request.user
+            post.save()
+            form.save_m2m() #ManyToManyField에 저장
+            return redirect('end.html')
+        else:
+            form = PostForm()
+        return render(request, 'post.html', {'form':form, 'select_hashtag':select_hashtag})
+
+
+
