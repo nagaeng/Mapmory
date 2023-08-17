de21f82b8cb24464e68692c041d7c4973773caf4
chaeyeon <devil5322@naver.com>
2023-08-17 20:39:44 +0900

    세팅 커밋

--- Mapmory/config/urls.py
+++ Mapmory/config/urls.py
@@ -5,10 +5,13 @@ urlpatterns = [
 
     
    
-
+    path('config_security/',views.config_secuity, name="config_security"),
     path('config_home/',views.config_home, name="config_home"),
-
     path('change_password/',views.change_password, name="re_password"),# 비밀번호 변경
     path('set_language/', views.set_language, name='set_language'), #언어 변경
     path('show_email/',views.show_email, name='show_email'), # 연결된 이메일
+    
+    path('dark_mode/',views.dark_mode, name='dark_mode'),
+    path('notification/',views.notification, name='notification'),
+    path('ko_en/', views.ko_en, name="ko_en"),
 ]
--- Mapmory/recommend/views.py
+++ Mapmory/recommend/views.py
@@ -1,13 +1,13 @@
 # views.py
 from django.shortcuts import redirect, render
-from post.models import Post,Tag
+from post.models import Post
 def search(request):
     if request.method == 'POST':
         keyword = request.POST.get('keyword')  # keyword를 입력받음
-        tag = Tag.objects.filter(tag_content=keyword)  # 해당 키워드를 가진 tag 클래스 오픈
-        if tag:
+        #tag = Tag.objects.filter(tag_content=keyword)  # 해당 키워드를 가진 tag 클래스 오픈
+        '''if tag:
             post = Post.objects.filter(hashtag__tag_content=keyword).order_by('-datetime')  # 해당 태그를 가진 post 저장
             return render(request, 'search_result.html', {'post': post, 'keyword': keyword})
         else:
-            return render(request, 'no_search.html')
+            return render(request, 'no_search.html')'''
     return render(request, 'button.html')
