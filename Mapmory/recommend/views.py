0827443b9ba9b4656855ed21ccbd51dd7a954350
nagaeng <109855280+nagaeng@users.noreply.github.com>
2023-08-18 02:51:06 +0900

    Update views.py
    주석 잘못된 거 처리ㅎㄷ요

--- Mapmory/recommend/views.py
+++ Mapmory/recommend/views.py
@@ -1,15 +1,4 @@
 
-# views.py
-from django.shortcuts import redirect, render
-from post.models import Post
-def search(request):
-    if request.method == 'POST':
-        keyword = request.POST.get('keyword')  # keyword를 입력받음
-        #tag = Tag.objects.filter(tag_content=keyword)  # 해당 키워드를 가진 tag 클래스 오픈
-        '''if tag:
-            post = Post.objects.filter(hashtag__tag_content=keyword).order_by('-datetime')  # 해당 태그를 가진 post 저장
-            return render(request, 'search_result.html', {'post': post, 'keyword': keyword})
-
 from django.shortcuts import render
 from post.models import Post
 from django.contrib.auth.decorators import login_required
